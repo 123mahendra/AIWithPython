3efbbea702b4abf2d1a47e653d0df54091f20019
Mahendra Gautam Sharma <mahendragautam464@gmail.com>
2025-09-28 23:18:41 +0300

    Assignment4, Exercise1, Task0-3 Completed.

---
+++ Assignment4/task1.py
@@ -0,0 +1,21 @@
+
+#####Task 1#####
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+valuesOfN = [500, 1000, 2000, 5000, 10000, 15000, 20000, 50000, 100000]
+
+for n in valuesOfN:
+    firstDice = np.random.randint(1, 7, n)
+    secondDice = np.random.randint(1, 7, n)
+
+    sumOfDices = firstDice + secondDice
+
+    h, h2 = np.histogram(sumOfDices, range(2, 14))
+
+    plt.bar(h2[:-1], h/n)
+
+    plt.title(f"Dice Sum Distribution (n={n})")
+
+    plt.show()
